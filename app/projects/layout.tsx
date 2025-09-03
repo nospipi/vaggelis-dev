55e47b36c789d6aa1fd1637f31b3fa5ecef3c8bb
Vaggelis Magonezos <nospipian@gmail.com>
2025-09-03 16:08:27 +0300

    initial commit

---
+++ app/projects/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return <div>{children}</div>;
+};
+
+export default Layout;
---
+++ app/projects/project-example/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Project Example Page</div>;
+};
+
+export default Page;
